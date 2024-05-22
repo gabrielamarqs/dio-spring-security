835a6bfe30ae4190b2e4c948ebe3292556617a8c
Gabriela Marques <gabriela2013.ms@gmail.com>
2024-05-22 09:50:57 -0300

    using adapter

--- auth-simples/src/main/java/gbrmrqs/dio/spring/security/with/dio/WelcomeController.java
+++ auth-simples/src/main/java/gbrmrqs/dio/spring/security/with/dio/WelcomeController.java
@@ -6,17 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+
     @GetMapping
     public String welcome(){
         return "Welcome to My Spring Boot Web API";
     }
+
     @GetMapping("/users")
-    @PreAuthorize("hasAnyRole('MANAGERS','USERS')")
     public String users() {
         return "Authorized user";
     }
+
     @GetMapping("/managers")
-    @PreAuthorize("hasRole('MANAGERS')")
     public String managers() {
         return "Authorized manager";
     }
